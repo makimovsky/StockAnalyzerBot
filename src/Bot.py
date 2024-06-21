e2ea48edec4d0af78e584acbc6e6b28922419164
makimovsky <makimofficial@gmail.com>
2024-06-21 10:38:10 +0200

    dodanie wskaźnika adx, dodanie opcji wyboru okresu czasu i interwału.

--- src/Bot.py
+++ src/Bot.py
@@ -1,27 +1,61 @@
 import logging
 import os
-import datetime
 import yfinance as yf
 
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from StockAnalysis import year_cycle_graph, rsi_so_price
+from StockAnalysis import year_cycle_graph, rsi_so_price, adx
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = ("*Cześć!*\nJestem botem, który pomaga analizować rynki finansowe.\n\n*Jak działam?*\nPobieram dane "
-            "z Yahoo Finance, analizuje je i na ich podstawie wyświetlam:\nTBD")
+            "z Yahoo Finance i na ich podstawie wyświetlam wykresy oraz wartości wskaźników.")
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.MARKDOWN)
 
 
 async def review(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    symbol = update.message.text.split(" ")[-1]
+    intervals = ['1m', '2m', '5m', '15m', '30m', '60m', '90m', '1d', '5d', '1wk', '1mo', '3mo']
 
-    year = datetime.datetime.now().year - 5
-    data = yf.download(symbol, start=f"{year}-01-01")
+    periods = {
+        '1d': intervals[0:3],
+        '5d': intervals[0:6],
+        '1mo': intervals[3:8],
+        '6mo': intervals[7:8],
+        '1y': intervals[7:10],
+        '2y': intervals[7:10],
+        '5y': intervals[7:11],
+        '10y': intervals[7:],
+    }
+
+    symbol = update.message.text.split(" ")[1]
+    try:
+        period = update.message.text.split(" ")[2]
+    except IndexError:
+        period = "1y"
+    try:
+        interval = update.message.text.split(" ")[3]
+    except IndexError:
+        interval = "1d"
+
+    if period not in periods:
+        err_msg = f"Błąd - dostępne okresy czasu: {str(list(periods.keys())).replace("'", "")}"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=err_msg, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    if interval not in intervals:
+        err_msg = f"Błąd - dostępne interwały: {str(intervals).replace("'", "")}"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=err_msg, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    if interval not in periods.get(period):
+        err_msg = f"Błąd - dostępne interwały dla okresu {period}: {str(periods.get(period)).replace("'", "")}"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=err_msg, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    data = yf.download(symbol, period=period, interval=interval)
 
     if data.empty:
         err_msg = f"Błąd - Brak danych o *{symbol}*"
@@ -38,8 +72,13 @@ async def review(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_photo(chat_id=update.effective_chat.id, photo="../resources/rop.png")
 
     # year cycle chart
-    year_cycle_graph(data)
-    await context.bot.send_photo(chat_id=update.effective_chat.id, photo="../resources/ycycle.png")
+    if period in list(periods.keys())[6:]:
+        year_cycle_graph(data)
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo="../resources/ycycle.png")
+
+    # ADX
+    adx(data)
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo="../resources/adx.png")
 
 
 load_dotenv()
--- src/StockAnalysis.py
+++ src/StockAnalysis.py
@@ -1,14 +1,11 @@
-import datetime
-
 import pandas as pd
 import matplotlib.pyplot as plt
+from ta import trend, momentum
 
 
 def save_fig(filename, title=""):
     plt.title(title)
     plt.legend()
-    plt.xticks([])
-    plt.yticks([])
     plt.xlabel('')
     plt.ylabel('')
     plt.tight_layout()
@@ -22,57 +19,41 @@ def year_cycle_graph(data: pd.DataFrame) -> None:
         data_year = data_year / data_year.max()
         data_year.index = data_year.index.strftime('%m-%d')
         data_year.plot(label=year)
+    plt.xticks([])
+    plt.yticks([])
     save_fig('ycycle.png', "Wykres cykli rocznych")
 
 
 def rsi_so_price(data: pd.DataFrame) -> tuple:
     # RSI
-    period = 14
-    delta = data['Close'].diff()
-
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)
-
-    avg_gain = gain.rolling(window=period).mean()
-    avg_loss = loss.rolling(window=period).mean()
-
-    rs = avg_gain / avg_loss
-
-    rsi = 100 - (100 / (1 + rs))
+    rsi = momentum.rsi(close=data["Close"], window=14)
+    rsi.dropna(inplace=True)
 
     avg_rsi = rsi.mean()
     std_rsi = rsi.std()
     act_rsi = rsi.iloc[-1]
 
     # Stochastics Oscilator
-    highest_high = data['High'].rolling(window=period).max()
-    lowest_low = data['Low'].rolling(window=period).min()
-
-    so = ((data['Close'] - lowest_low) / (highest_high - lowest_low)) * 100
+    so = momentum.stoch(high=data['High'], low=data['Low'], close=data['Close'], window=14, smooth_window=3)
+    so.dropna(inplace=True)
 
     avg_so = so.mean()
     std_so = so.std()
     act_so = so.iloc[-1]
 
     # chart
-    end = datetime.datetime.now()
-    start = end - datetime.timedelta(days=365)
-
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
 
     ax1.plot(data['Close'], color="red")
-    ax1.set_xlim(start, end)
     ax1.set_ylabel('Cena')
 
     ax2.plot(rsi, color="orange")
-    ax2.set_xlim(start, end)
     ax2.axhline(y=avg_rsi + std_rsi, color="orange")
     ax2.axhline(y=avg_rsi - std_rsi, color="orange")
     ax2.axhline(y=avg_rsi, color="orange", linestyle="--")
     ax2.set_ylabel('RSI')
 
     ax3.plot(so, color="blue")
-    ax3.set_xlim(start, end)
     ax3.axhline(y=avg_so + std_so, color="blue")
     ax3.axhline(y=avg_so - std_so, color="blue")
     ax3.axhline(y=avg_so, color="blue", linestyle="--")
@@ -84,3 +65,15 @@ def rsi_so_price(data: pd.DataFrame) -> tuple:
 
     return round(avg_rsi, 2), round(std_rsi, 2), round(act_rsi, 2), round(avg_so, 2), round(std_so, 2), \
         round(act_so, 2)
+
+
+def adx(data: pd.DataFrame) -> None:
+    c_adx = trend.adx(high=data['High'], low=data['Low'], close=data['Close'], window=14)
+    c_adx = c_adx[c_adx != 0.0]
+
+    # chart
+    c_adx.plot(label="ADX", color="black")
+    plt.axhline(y=20, color="green", label="Linia przebicia")
+    plt.axhline(y=40, color="red", linestyle="--", label="Linia uwagi")
+
+    save_fig("adx.png", "Wskaźnik trendu - ADX")
